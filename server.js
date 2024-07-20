223dfd768906b21f4a1cd66ccfd1cc37e93217d9
Premium Packs <155005470+premiumpacks@users.noreply.github.com>
2024-07-20 09:00:59 +0000

    changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Roteamento para adicionar a extensão .html
-app.get('/*', (req, res) => {
-  const filePath = path.join(__dirname, 'public', req.path + '.html');
+// Roteamento para lidar com URLs que não correspondem a arquivos específicos
+app.get('*', (req, res) => {
+  const filePath = path.join(__dirname, 'public', req.path);
     res.sendFile(filePath, (err) => {
         if (err) {
               // Se o arquivo não for encontrado, servir o index.html
