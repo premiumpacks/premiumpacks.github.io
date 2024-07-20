e5102a525c0dc96901aaad1e4cca2a9099dca39e
Premium Packs <155005470+premiumpacks@users.noreply.github.com>
2024-07-20 09:09:53 +0000

    changes

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const port = process.env.PORT || 3000;
 // Middleware para servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Roteamento para lidar com URLs que não correspondem a arquivos específicos
+// Serve o index.html para qualquer rota não especificada
 app.get('*', (req, res) => {
-  // Serve o index.html para qualquer URL que não seja um arquivo estático
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
-    app.listen(port, () => {
-      console.log(`Servidor rodando em http://localhost:${port}`);
-      });
-      
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+    
