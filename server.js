f50cf860b270e9858b0e091aeacfe5f85daa18e4
Premium Packs <155005470+premiumpacks@users.noreply.github.com>
2024-07-20 08:59:03 +0000

    changes

--- server.js
+++ server.js
@@ -3,25 +3,21 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware para servir arquivos estáticos
+// Middleware para servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Roteamento para remover a extensão .html
-app.get('/*.html', (req, res) => {
-  const filePath = path.join(__dirname, 'public', req.path);
-    res.sendFile(filePath);
-    });
-
-    app.get('/*', (req, res) => {
-      const filePath = path.join(__dirname, 'public', req.path + '.html');
-        res.sendFile(filePath, (err) => {
-            if (err) {
-                  res.status(404).send('Página não encontrada');
-                      }
-                        });
-                        });
-
-                        app.listen(port, () => {
-                          console.log(`Servidor rodando em http://localhost:${port}`);
+// Roteamento para adicionar a extensão .html
+app.get('/*', (req, res) => {
+  const filePath = path.join(__dirname, 'public', req.path + '.html');
+    res.sendFile(filePath, (err) => {
+        if (err) {
+              // Se o arquivo não for encontrado, servir o index.html
+                    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+                        }
                           });
-                          
+                          });
+
+                          app.listen(port, () => {
+                            console.log(`Servidor rodando em http://localhost:${port}`);
+                            });
+                            
